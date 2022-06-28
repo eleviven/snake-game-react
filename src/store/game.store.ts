6d08ef85f11d5cbe79151bcf0e0bd71787c4df45
Firuz Hacıyev <firuz.haciyeff@gmail.com>
2022-06-29 00:17:50 +0400

    Snake intersect and border check

--- src/store/game.store.ts
+++ src/store/game.store.ts
@@ -1,41 +1,59 @@
 import create from "zustand";
-import { BOARD_SIZE, DIRECTIONS, PIXEL_SIZE } from "../constants";
-import { ICoords } from "../types";
+import {
+  BOARD_SIZE,
+  DIRECTIONS,
+  DIRECTIONS_REVERSE,
+  PIXEL_SIZE,
+} from "../constants";
+import { GAME_STATUSES, ICoords } from "../types";
 import { generateRandomCoords } from "../utils";
 
 export interface IGameStore {
-  status: boolean;
+  status: GAME_STATUSES;
   direction: null | keyof typeof DIRECTIONS;
   snakeTails: ICoords[];
   appleCoords: null | ICoords;
-  setStatus: (params: boolean) => void;
+  score: number;
+  setStatus: (params: GAME_STATUSES) => void;
   initGame: () => void;
   moveSnake: () => void;
   setDirection: (params: keyof typeof DIRECTIONS) => void;
   createApple: () => void;
   checkIfEat: () => boolean | ICoords;
+  checkIfCross: () => boolean;
+  checkIfBorder: () => boolean;
   eatApple: () => void;
 }
 
 export const useGameStore = create<IGameStore>((set, get) => ({
   // values
-  status: false,
+  status: GAME_STATUSES.STOP,
   direction: null,
   snakeTails: [],
   appleCoords: null,
+  score: 0,
   // actions
-  setStatus: (status: boolean) => set(() => ({ status })),
+  setStatus: (status: GAME_STATUSES) => set(() => ({ status })),
   initGame: () =>
     set(() => ({
-      status: false,
+      status: GAME_STATUSES.STOP,
       direction: null,
       snakeTails: [
-        { x: 0, y: 0 },
-        { x: PIXEL_SIZE, y: 0 },
+        { x: 4 * PIXEL_SIZE, y: 4 * PIXEL_SIZE },
+        { x: 5 * PIXEL_SIZE, y: 4 * PIXEL_SIZE },
       ],
       appleCoords: generateRandomCoords(BOARD_SIZE),
+      score: 0,
     })),
-  setDirection: (payload) => set(() => ({ direction: payload })),
+  setDirection: (payload) => {
+    const { direction } = get();
+    const isReverse = direction && payload === DIRECTIONS_REVERSE[direction];
+
+    if (!isReverse)
+      set(() => {
+        return { direction: payload };
+      });
+  },
   moveSnake: () =>
     set((state) => {
       const tails = [...state.snakeTails];
@@ -69,12 +87,11 @@ export const useGameStore = create<IGameStore>((set, get) => ({
   eatApple: () =>
     set((state) => {
       const tails = [...state.snakeTails];
-      if (state.appleCoords) {
-        tails.unshift({ x: state.appleCoords?.x, y: state.appleCoords?.y });
-      }
+      tails.unshift({ x: state.snakeTails[0]?.x, y: state.snakeTails[0]?.y });
       get().createApple();
       return {
         snakeTails: tails,
+        score: state.score + 1,
       };
     }),
   checkIfEat: () => {
@@ -86,5 +103,46 @@ export const useGameStore = create<IGameStore>((set, get) => ({
       get().eatApple();
     }
     return isEated && appleCoords ? appleCoords : false;
+  },
+  checkIfCross: () => {
+    const snakeTails = [...get().snakeTails];
+    const head = snakeTails.pop();
+
+    if (!head) return false;
+
+    const crossedCoordsIndex = snakeTails.findIndex(
+      (tail) => tail.x === head.x && tail.y === head.y
+    );
+
+    if (crossedCoordsIndex > 0) {
+      const newSnakeTails = snakeTails.filter(
+        (_, index) => index >= crossedCoordsIndex
+      );
+      set((state) => ({
+        snakeTails: newSnakeTails,
+        score: Math.floor(state.score / 2),
+      }));
+    }
+
+    return false;
+  },
+  checkIfBorder: () => {
+    const { setStatus } = get();
+    const snakeTails = [...get().snakeTails];
+    const head = snakeTails.pop();
+
+    if (!head) return false;
+
+    const isBorder =
+      head.x < 0 ||
+      head.x > BOARD_SIZE * PIXEL_SIZE - PIXEL_SIZE ||
+      head.y < 0 ||
+      head.y > BOARD_SIZE * PIXEL_SIZE - PIXEL_SIZE;
+
+    if (isBorder) {
+      setStatus(GAME_STATUSES.END);
+    }
+
+    return isBorder;
   },
 }));
